ff5f404683ae336887d10dbabf82bc9649090767
M.Zaki Al Akkari <MrZakiakkari@users.noreply.github.com>
2020-07-17 22:00:35 +0100

    SQLite Implementation

---
+++ BambooOnlineStore.Repositories.SQLite/BambooOnlineStoreDatabase.cs
@@ -0,0 +1,22 @@
+﻿namespace BambooOnlineStore.Repositories.SQLite
+{
+	using System;
+	public partial class BambooOnlineStoreDatabase : BambooOnlineStoreSystem, DatabaseAgent
+	{
+		public BambooOnlineStoreDatabase(String connectionString)
+		{
+			this.ConnectionString = connectionString;
+			var timeFactorsSqlStatements = new BambooOnlineStoreSqlStatements();
+			//
+			//	repositories
+			//
+			this.UserAccountRepository = new UserAccountRepositoryComponent(this, timeFactorsSqlStatements);
+			this.UserGroupRepository = new UserGroupRepositoryComponent(this, timeFactorsSqlStatements);
+		}
+		#region Public Properties
+		#endregion
+		public UserAccountRepository UserAccountRepository { get; }
+		public UserGroupRepository UserGroupRepository { get; }
+		public String ConnectionString { get; }
+	}
+}
